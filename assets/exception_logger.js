1449487d7b6a0d524489439a91e3e8a4129bc527
technoweenie <technoweenie@567b1171-46fb-0310-a4c9-b4bef9110e78>
2006-06-26 04:12:57 +0000

    add activity indicator
    git-svn-id: http://svn.techno-weenie.net/projects/plugins/exception_logger@1275 567b1171-46fb-0310-a4c9-b4bef9110e78

--- assets/exception_logger.js
+++ assets/exception_logger.js
@@ -26,3 +26,13 @@ Event.observe(window, 'load', function() {
     $(context + '_filter').value = '';
   });
 });
+
+Ajax.Responders.register({
+  onCreate: function() {
+    if($('activity') && Ajax.activeRequestCount > 0) $('activity').visualEffect('appear', {duration:0.25});
+  },
+
+  onComplete: function() {
+    if($('activity') && Ajax.activeRequestCount == 0) $('activity').visualEffect('fade', {duration:0.25});
+  }
+});
