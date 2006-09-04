e2d703cdf6d96bbb2b21eeb4a3bedcd1cfc65be5
technoweenie <technoweenie@567b1171-46fb-0310-a4c9-b4bef9110e78>
2006-09-04 03:57:56 +0000

    add RSS feed support
    git-svn-id: http://svn.techno-weenie.net/projects/plugins/exception_logger@2038 567b1171-46fb-0310-a4c9-b4bef9110e78

--- lib/logged_exceptions_controller.rb
+++ lib/logged_exceptions_controller.rb
@@ -1,4 +1,5 @@
 class LoggedExceptionsController < ActionController::Base
+  cattr_accessor :application_name
   layout nil
 
   def index
@@ -10,6 +11,10 @@ class LoggedExceptionsController < ActionController::Base
   def query
     conditions = []
     parameters = []
+    unless params[:id].blank?
+      conditions << 'id = ?'
+      parameters << params[:id]
+    end
     unless params[:query].blank?
       conditions << 'message LIKE ?'
       parameters << "%#{params[:query]}%"
@@ -28,6 +33,12 @@ class LoggedExceptionsController < ActionController::Base
     end
     @exception_pages, @exceptions = paginate :logged_exceptions, :order => 'created_at desc', :per_page => 30, 
       :conditions => conditions.empty? ? nil : parameters.unshift(conditions * ' and ')
+    
+    respond_to do |format|
+      format.html { redirect_to :action => 'index' unless action_name == 'index' }
+      format.js
+      format.rss  { render :action => 'query.rxml' }
+    end
   end
   
   def show
@@ -42,4 +53,30 @@ class LoggedExceptionsController < ActionController::Base
     LoggedException.delete_all ['id in (?)', params[:ids]] unless params[:ids].blank?
     query
   end
+
+  private
+    def access_denied_with_basic_auth
+      headers["Status"]           = "Unauthorized"
+      headers["WWW-Authenticate"] = %(Basic realm="Web Password")
+      render :text => "Could't authenticate you", :status => '401 Unauthorized'
+    end
+
+    # gets BASIC auth info
+    def get_auth_data
+      user, pass = '', '' 
+      # extract authorisation credentials 
+      if request.env.has_key? 'X-HTTP_AUTHORIZATION' 
+        # try to get it where mod_rewrite might have put it 
+        authdata = request.env['X-HTTP_AUTHORIZATION'].to_s.split 
+      elsif request.env.has_key? 'HTTP_AUTHORIZATION' 
+        # this is the regular location 
+        authdata = request.env['HTTP_AUTHORIZATION'].to_s.split  
+      end 
+       
+      # at the moment we only support basic authentication 
+      if authdata && authdata[0] == 'Basic' 
+        user, pass = Base64.decode64(authdata[1]).split(':')[0..1] 
+      end 
+      return [user, pass] 
+    end
 end
