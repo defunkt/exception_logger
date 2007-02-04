c7c487b03d237f0f4894ba3b4507f90256a1f3eb
technoweenie <technoweenie@567b1171-46fb-0310-a4c9-b4bef9110e78>
2007-02-04 22:43:20 +0000

    hell, it already requires edge anyway
    git-svn-id: http://svn.techno-weenie.net/projects/plugins/exception_logger@2732 567b1171-46fb-0310-a4c9-b4bef9110e78

--- init.rb
+++ init.rb
@@ -1,5 +1,1 @@
-if LoggedExceptionsController.respond_to?(:view_paths)
-  LoggedExceptionsController.view_paths = [File.join(directory, 'views')]
-else
-  LoggedExceptionsController.template_root = File.join(directory, 'views')
-end
+LoggedExceptionsController.view_paths = [File.join(directory, 'views')]
