e700502105f93959f8cdfe01cebabb65c05eea60
technoweenie <technoweenie@567b1171-46fb-0310-a4c9-b4bef9110e78>
2007-02-04 22:42:02 +0000

    use LoggedExceptionsController#view_paths if available (edge rails only)
    git-svn-id: http://svn.techno-weenie.net/projects/plugins/exception_logger@2731 567b1171-46fb-0310-a4c9-b4bef9110e78

--- init.rb
+++ init.rb
@@ -1,1 +1,5 @@
-LoggedExceptionsController.template_root = File.join(directory, 'views')
+if LoggedExceptionsController.respond_to?(:view_paths)
+  LoggedExceptionsController.view_paths = [File.join(directory, 'views')]
+else
+  LoggedExceptionsController.template_root = File.join(directory, 'views')
+end
