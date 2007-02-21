1b74e2d562a4ba2a2e882ab2cfc5b4994640678b
technoweenie <technoweenie@567b1171-46fb-0310-a4c9-b4bef9110e78>
2007-02-21 16:14:40 +0000

    update logged_exception message column to TEXT datatype
    git-svn-id: http://svn.techno-weenie.net/projects/plugins/exception_logger@2753 567b1171-46fb-0310-a4c9-b4bef9110e78

--- generators/exception_migration/templates/migration.rb
+++ generators/exception_migration/templates/migration.rb
@@ -4,7 +4,7 @@ class <%= class_name %> < ActiveRecord::Migration
       t.column :exception_class, :string
       t.column :controller_name, :string
       t.column :action_name,     :string
-      t.column :message,         :string
+      t.column :message,         :text
       t.column :backtrace,       :text
       t.column :environment,     :text
       t.column :request,         :text
